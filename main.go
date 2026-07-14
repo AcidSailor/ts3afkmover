07911e4176a1c9f3d02e8152494a2d1a83d3ab92
Denis Shaporov <d@shaporov.dev>
2026-07-14 21:16:16 +0300

    style: satisfy go-scaffolds golangci config (gofumpt, staticcheck, errcheck)

--- main.go
+++ main.go
@@ -1,63 +1,65 @@
 package main
 
 import (
-    "context"
-    "github.com/acidsailor/ts3afkmover/configs"
-    "github.com/acidsailor/ts3afkmover/internal/controllers"
-    "github.com/acidsailor/ts3afkmover/internal/usecases"
-    "log/slog"
-    "os"
-    "os/signal"
-    "sync"
-    "syscall"
-    "time"
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/acidsailor/ts3afkmover/configs"
+	"github.com/acidsailor/ts3afkmover/internal/controllers"
+	"github.com/acidsailor/ts3afkmover/internal/usecases"
 )
 
 func main() {
-    logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-        AddSource: true,
-    }))
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
 
-    config, err := configs.NewConfig()
+	config, err := configs.NewConfig()
+	if err != nil {
+		logger.Error(
+			"error initializing config",
+			slog.String("err", err.Error()),
+		)
+		os.Exit(1)
+	}
 
-    if err != nil {
-        logger.Error(
-            "error initializing config",
-            slog.String("err", err.Error()),
-        )
-        os.Exit(1)
-    }
+	idleUsecase := usecases.NewTs3IdleUsecase(
+		config, logger, controllers.NewTs3Client(
+			config,
+			logger,
+		),
+	)
 
-    idleUsecase := usecases.NewTs3IdleUsecase(
-        config, logger, controllers.NewTs3Client(
-            config,
-            logger,
-        ),
-    )
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
+	var wg sync.WaitGroup
+	ticker := time.NewTicker(
+		time.Duration(config.IdleCheckInterval) * time.Minute,
+	)
+	sigs := make(chan os.Signal, 1)
 
-    var wg sync.WaitGroup
-    ticker := time.NewTicker(time.Duration(config.IdleCheckInterval) * time.Minute)
-    sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-    signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	logger.InfoContext(ctx, "starting application")
 
-    logger.InfoContext(ctx, "starting application")
-
-    wg.Add(1)
-    go func() {
-        defer wg.Done()
-        defer ticker.Stop()
-        for {
-            select {
-            case <-sigs:
-                return
-            case <-ticker.C:
-                idleUsecase.MoveIdleClients(ctx)
-            }
-        }
-    }()
-    wg.Wait()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer ticker.Stop()
+		for {
+			select {
+			case <-sigs:
+				return
+			case <-ticker.C:
+				idleUsecase.MoveIdleClients(ctx)
+			}
+		}
+	}()
+	wg.Wait()
 }
